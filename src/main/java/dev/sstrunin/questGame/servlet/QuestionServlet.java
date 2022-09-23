fe0aa5a48218c50d90b3b09c999426ba9c235836
sergey <segich94@gmail.com>
2022-09-23 16:30:06 +0300

    changes servlets

--- src/main/java/dev/sstrunin/questGame/servlet/QuestionServlet.java
+++ src/main/java/dev/sstrunin/questGame/servlet/QuestionServlet.java
@@ -3,6 +3,8 @@ package dev.sstrunin.questGame.servlet;
 import dev.sstrunin.questGame.entity.Question;
 import dev.sstrunin.questGame.entity.User;
 import dev.sstrunin.questGame.repository.QuestionsRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -14,7 +16,7 @@ import java.io.IOException;
 
 @WebServlet(name = "questionServlet", value = "/question")
 public class QuestionServlet extends HttpServlet {
-
+    private static final Logger LOGGER = LogManager.getLogger(QuestionServlet.class);
     private QuestionsRepository questionsRepository;
 
     @Override
@@ -32,7 +34,7 @@ public class QuestionServlet extends HttpServlet {
         req.setAttribute("question", question.getQuestion());
         req.setAttribute("answer1", question.getAnswer1());
         req.setAttribute("answer2", question.getAnswer2());
-
+        LOGGER.info("Question: {} User: {} Session: {}",question.getQuestion(),user,req.getSession());
         getServletContext().getRequestDispatcher("/question/question.jsp").forward(req, resp);
     }
 
