f3949dd820ac1dcfcbcccdb0bf0621655f9cacd0
sergey <segich94@gmail.com>
2022-09-23 16:04:02 +0300

    From work notebook

--- src/main/java/dev/sstrunin/questGame/servlet/StartServlet.java
+++ src/main/java/dev/sstrunin/questGame/servlet/StartServlet.java
@@ -13,10 +13,11 @@ import java.io.IOException;
 
 @WebServlet(name = "startServlet", value = "/start")
 public class StartServlet extends HttpServlet {
-
+    private final static Logger LOGGER = LogManager.getLogger(StartServlet.class);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        LOGGER.info("Connect: session {}",req.getSession());
         resp.setContentType("text/html;charset=UTF-8");
         RequestDispatcher requestDispatcher = req.getRequestDispatcher("start/start.jsp");
         requestDispatcher.forward(req, resp);
